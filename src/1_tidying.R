6cd5f21391328552c6d68163e28268ca7e1b7e5d
charlVSchoor <31816867+charlVSchoor@users.noreply.github.com>
2018-03-19 08:52:34 +0200

    Update 3
    Just some cleaner code

--- src/1_tidying.R
+++ src/1_tidying.R
@@ -4,11 +4,13 @@ library(tidytext)
 library(topicmodels)
 library(plotly)
 library(tm)
+library(lubridate)
 # --------------------------------- #
 OWN_STOP_WORDS = c("In", "cent", "4,5", "5,7", "r1290", "2,3", "1,6", "16,5", "93")
-TOP_WORDS_TO_GRAPH = 10
-NUM_TOPICS = 7
-YEARS = 2017:2018
+TOP_WORDS_TO_GRAPH = 6
+NUM_TOPICS = 30
+YEARS = 2013:2018
+NUM_DOCUMENTS = 31
 # --------------------------------- #
 setwd("/Users/charlvanschoor/Documents/GitHub/Viegi/src")
 writeLines(paste0("Running 1_tidying.R"))
@@ -17,8 +19,8 @@ writeLines(paste0("Running 1_tidying.R"))
 # dataframe construction
 mpc_extract <- 0
 for (i in YEARS) {
-  i <- as.tibble(read.csv(paste0("~/Documents/GitHub/Viegi/src/data/text_extract/", i,"/mpc_text_extract_", i,".csv")))
-  mpc_extract <- rbind(mpc_extract, i) %>% filter(!is.na(bank))
+  i <- as.tibble(read_csv(paste0("~/Documents/GitHub/Viegi/src/data/text_extract/", i,"/mpc_text_extract_", i,".csv")))
+  mpc_extract <- rbind(mpc_extract, i) %>% filter(!is.na(bank), bank != 0)
 }
 print(mpc_extract)
 
@@ -27,7 +29,8 @@ mpc_extract_clean <- mpc_extract %>%
   mutate(nr = 1:length(bank),
          bank = as.factor(bank),
          month = as.integer(month),
-         mpc_communication = as.character(mpc_communication)) %>%
+         mpc_communication = as.character(mpc_communication),
+         date = dmy(date)) %>%
   select(nr, everything()) 
 print(mpc_extract_clean)
 
@@ -39,7 +42,8 @@ tidy_mpc_extract <- mpc_extract_clean %>%
 print(tidy_mpc_extract)
 str(tidy_mpc_extract)
 
-# proportion of each word in the document (the distribution of the documents):
+# words in each document by percentage of appearance (the distribution of the documents):
+#### GRAPH 1
 tidy_mpc_extract %>% 
   group_by(word, nr) %>%
   summarise(count = n()) %>% 
@@ -54,7 +58,8 @@ tidy_mpc_extract %>%
                                           axis.text.y=element_blank(),
                                           axis.ticks.y=element_blank())
 
-# proportion of each word in the document, above 10 count representation:
+# most common words in each document by percentage of appearance:
+#### GRAPH 2
 tidy_mpc_extract %>% 
   group_by(word, nr) %>%
   summarise(count = n()) %>% 
@@ -62,7 +67,7 @@ tidy_mpc_extract %>%
   group_by(nr) %>%
   mutate(prop = count/sum(count)) %>%
   ungroup() %>% 
-  filter(count > 10) %>% 
+  filter(count > 15) %>% 
   filter(!(word %in% OWN_STOP_WORDS)) %>%
   arrange(nr, -prop) %>% 
   mutate(word = reorder(word, prop), nr = as.factor(nr)) %>% 
@@ -96,35 +101,65 @@ print(mpc_topics_gamma)
 # --------------------------------- #
 
 # graphing top words, by probability, in each topic
+# top words
 mpc_top_terms <- mpc_topics_beta %>%
   group_by(topic) %>% 
-  top_n(TOP_WORDS_TO_GRAPH, -beta) %>% 
+  top_n(TOP_WORDS_TO_GRAPH, beta) %>% 
   ungroup() %>%
   arrange(topic, -beta)
 print(mpc_top_terms)
 
+# bottom words
+mpc_bottom_terms <- mpc_topics_beta %>%
+  group_by(topic) %>% 
+  top_n(TOP_WORDS_TO_GRAPH, -beta) %>% 
+  ungroup() %>%
+  arrange(topic, -beta)
+print(mpc_bottom_terms)
+
+#### GRAPH 3
 mpc_top_terms %>% 
   mutate(term = reorder(term, beta)) %>% 
   ggplot(aes(term, beta, fill = factor(topic))) +
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
   coord_flip() +  theme(axis.title.x=element_blank(),
-                        axis.ticks.x=element_blank())
+                        axis.ticks.x=element_blank(),
+                        axis.text.x = element_blank())
+#### GRAPH 4
+mpc_top_terms %>% 
+  mutate(term = reorder(term, beta)) %>% 
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_col(show.legend = FALSE) +
+  facet_grid(~ topic, scales = "free") +
+  coord_flip() +  theme(axis.title.x=element_blank(),
+                        axis.ticks.x=element_blank(),
+                        axis.text.x = element_blank())
 
-
-#get topic distributions per document
+#get topic distributions per document - which top 3 topic does each document likely belong to
 mpc_top_documents <- mpc_topics_gamma %>%
   group_by(document) %>% 
-  top_n(1, gamma) %>%
+  top_n(10, gamma) %>%
   ungroup() %>% 
-  mutate(topic = as.factor(topic)) %>% 
-  arrange(topic, -gamma)
+  mutate(topic = as.factor(topic),
+         document = factor(document, levels = c(1:NUM_DOCUMENTS))) %>% 
+  arrange(document, topic, -gamma)
 print(mpc_top_documents)
 
-mpc_top_documents %>%
-  ggplot() + geom_col(aes(x=document, y=gamma, col=topic, fill=topic)) +
-  theme(axis.text.y = element_blank())
 
+#### GRAPH 5
+mpc_top_documents %>% 
+  ggplot() + geom_col(aes(x=document, y=gamma, col=topic, fill=topic)) +
+  theme(axis.text.y = element_blank()) 
+
+#### GRAPH 6
+mpc_top_documents %>% 
+  mutate(gamma = reorder(gamma, document)) %>% 
+  ggplot() + geom_col(aes(x=document, y=gamma, col=topic, fill=topic, alpha = 1/3)) +
+  theme(axis.text.y = element_blank()) 
+
+
+# Next -0 sum all the gammas for each document - get the distributions of each
 
 
 
